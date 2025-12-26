a61bf189faec823ed00c7ba93dacc7353dfc9bdd
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2025-12-26 23:53:23 +0530

    Time: 1 ms (92.36%) | Memory: 46.9 MB (34.44%) - LeetSync

---
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int earliestTime(int[][] tasks) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<tasks.length; i++){
+            int sum = tasks[i][0] + tasks[i][1];
+            min = Math.min(min, sum);
+        }
+        return min;
+    }
+}
