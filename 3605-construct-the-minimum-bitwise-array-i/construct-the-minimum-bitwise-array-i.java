d0726f4c782d4b1b49d3444f0953f6a65f741c4b
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-01-20 22:59:23 +0530

    Time: 28 ms (11.40%) | Memory: 46.9 MB (30.70%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] ans = new int[nums.size()];
+        for(int i = 0; i<nums.size(); i++){
+            double sqrt = Math.sqrt(nums.get(i)+1);
+            if(sqrt%1 == 0){
+                ans[i] = nums.get(i)/2;
+                continue;
+            }
+            boolean check = true;
+            for(int j = 1; j<nums.get(i); j++){
+                if((j | (j+1)) == nums.get(i)) {
+                    ans[i] = j;
+                    check = false;
+                    break;
+                }
+            }
+            if(!check) continue;
+            ans[i] = -1;
+        }
+        return ans;
+    }
+}
