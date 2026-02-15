705d9580e7d95ff5e4d0a42ab9a85834e5882b95
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-02-15 23:38:21 +0530

    Time: 1 ms (98.25%) | Memory: 43.6 MB (64.89%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.java
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -3,10 +3,11 @@ class Solution {
         int even = 0;
         int odd = 0;
         for(int i = 0; i<num.length(); i++){
+            char ch = num.charAt(i);
             if(i%2 == 0){
-                even += num.charAt(i) - '0';
+                even += ch - '0';
             }else{
-                odd += num.charAt(i) - '0';
+                odd += ch - '0';
             }
         }
         return even == odd;
