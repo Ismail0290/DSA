fe286e360371c957ec6eb92f431e16835b083e75
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-03-03 22:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (96.14%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,34 +1,12 @@
 class Solution {
     public boolean check(int[] nums) {
+        int count = 0;
         int n = nums.length;
-        int[] arr = new int[n];
-        for(int x = 0; x<n; x++){
-            for(int i = 0; i<n; i++){
-                arr[i] = nums[(i+x) % n];
+        for(int i = 0; i<n; i++){
+            if(nums[i] > nums[(i+1) % n]){
+                count++;
             }
-            if(isSorted(arr)) return true;
         }
-        return false;
-
-        // if(nums[0] <= nums[nums.length-1]){
-        //     for(int i = 0; i<nums.length-1; i++){
-        //         if(nums[i] > nums[i+1]) return false;
-        //     }
-        //     return true;
-        // }
-        // for(int i = 0; i<nums.length-1; i++){
-        //     boolean peaked = false;
-        //     if(nums[i] > nums[i+1]){
-        //         if(peaked) return false;
-        //         peaked = true;
-        //     }
-        // }
-        // return true;
-    }
-    static boolean isSorted(int[] arr){
-        for(int i = 0; i<arr.length-1; i++){
-            if(arr[i] > arr[i+1]) return false;
-        }
-        return true;
+        return count <= 1;
     }
 }
