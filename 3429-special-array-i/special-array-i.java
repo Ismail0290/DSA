cb1acae71fcfa32d205991888995d024030db4eb
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-03-14 21:18:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (34.14%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums[0] % 2 == 0) {
+            for(int i = 0; i<nums.length; i++){
+                if(i%2 == 0 && nums[i]%2 != 0) return false;
+                if(i%2 != 0 && nums[i]%2 == 0) return false;
+            }
+        }else{
+            for(int i = 0; i<nums.length; i++){
+                if(i%2 == 0 && nums[i]%2 == 0) return false;
+                if(i%2 != 0 && nums[i]%2 != 0) return false;
+            }
+        }
+        return true;
+    }
+}
