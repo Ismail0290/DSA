1930c3276b74e843d3418ec713760379ba95973a
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-04-29 00:44:00 +0530

    Time: 45 ms (43.18%) | Memory: 46.8 MB (54.34%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
@@ -3,18 +3,16 @@ class Solution {
         int count = 0;
         for(int i = 0; i<words.length; i++){
             for(int j = i+1; j<words.length; j++){
-                if(isSimilar(words[i], words[j])) count++;
+                if(getMask(words[i]) == getMask(words[j])) count++;
             }
         }
         return count;
     }
-    static boolean isSimilar(String s1, String s2){
-        HashSet<Character> set1 = new HashSet<>();
-        HashSet<Character> set2 = new HashSet<>();
-
-        for(char c : s1.toCharArray()) set1.add(c);
-        for(char c : s2.toCharArray()) set2.add(c);
-
-        return set1.equals(set2);
+    static int getMask(String s){
+        int mask = 0;
+        for(char ch: s.toCharArray()){
+            mask |= 1 << (ch-'a');
+        }
+        return mask;
     }
 }
