b671ccd9ce83a678973ce6d193051e60f6abc83a
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-05-26 00:15:53 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (70.66%) - LeetSync

--- 4295-count-indices-with-opposite-parity/count-indices-with-opposite-parity.java
+++ 4295-count-indices-with-opposite-parity/count-indices-with-opposite-parity.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int[] countOppositeParity(int[] nums) {
+class Solution 
+{
+    public int[] countOppositeParity(int[] nums) 
+    {
         int n = nums.length;
-        for(int i = 0; i<n; i++){
-            int count = 0;
-            if(nums[i] % 2 == 0){
-                for(int j = i+1; j<n; j++){
-                    if(nums[j] % 2 != 0) count++;
-                }
-                nums[i] = count;
-            }else{
-                for(int j = i+1; j<n; j++){
-                    if(nums[j] % 2 == 0) count++;
-                }
-                nums[i] = count;
+        int[] answer = new int[n];
+        int evens = 0;
+        int odds = 0;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]%2==0){
+                answer[i]=odds;
+                evens++;
+            } else {
+                answer[i]=evens;
+                odds++;
             }
         }
-        return nums;
+        return answer;
     }
 }
