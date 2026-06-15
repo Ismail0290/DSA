e56965649260aee80cc761f680c84d76ef43cce2
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-06-16 00:36:35 +0530

    Time: 12 ms (46.13%) | Memory: 46.3 MB (50.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,29 +5,17 @@ class Solution {
         int n = s.length();
         int max = 0;
         int ans = 1;
-        char maxChar = 'A';
-        HashMap<Character, Integer> map = new HashMap<>();
-        boolean changeRight = true;
-        while(left < n && right < n && left <= right){
-            if(changeRight){
-                map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0)+1);
-            }
-            if(map.get(s.charAt(right)) > max){
-                max = map.get(s.charAt(right));
-                maxChar = s.charAt(right);
-            }
-            if(k >= right-left+1-max){
-                ans = Math.max(right-left+1, ans);
-                right++;
-                changeRight = true;
-            }else{
-                map.put(s.charAt(left), map.get(s.charAt(left))-1);
-                max = 0;
-                for(int freq : map.values()){
-                    max = Math.max(max, freq);
-                }
+        int[] freq = new int[26];
+        while(right < n){
+            freq[s.charAt(right)-'A']++;
+            max = Math.max(freq[s.charAt(right) - 'A'], max);
+            if((right - left + 1) - max > k){
+                freq[s.charAt(left) - 'A']--;
                 left++;
-                changeRight = false;
+            }
+            if((right - left + 1) - max <= k){
+                ans = Math.max(right - left + 1, ans);
+                right++;
             }
         }
         return ans;
