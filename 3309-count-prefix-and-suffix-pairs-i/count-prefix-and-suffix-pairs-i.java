d20217395e2df9998780348d129cecfd6d9fa0bd
Ismail Sheikh <ismail.sheikh29dev@gmail.com>
2026-07-07 23:17:41 +0530

    Time: 3 ms (33.18%) | Memory: 43.9 MB (70.49%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int count = 0;
+        for(int i = 0; i<words.length; i++){
+            for(int j = i+1; j<words.length; j++){
+                if(isPrefixAndSuffix(words[i], words[j])){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    static boolean isPrefixAndSuffix(String s1, String s2){
+        if(s2.startsWith(s1) && s2.endsWith(s1)) return true;
+        return false;
+    }
+}
